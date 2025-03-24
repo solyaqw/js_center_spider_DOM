d93cf16165f464d5d7a1b399c47dbf9ded0d6be8
Solomiia Palka <solyaqw@gmail.com>
2025-03-24 13:39:32 +0200

    added some changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,30 @@
 'use strict';
 
+const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 
 function centerSpider() {
+  wall.style.position = 'relative';
+
   spider.style.position = 'absolute';
-  spider.style.top = '50%';
-  spider.style.left = '50%';
-  spider.style.transform = 'translate(-50%, -50%)';
+
+  const wallWidth = wall.clientWidth;
+  const wallHeight = wall.clientHeight;
+  const spiderWidth = spider.clientWidth;
+  const spiderHeight = spider.clientHeight;
+
+  const left = (wallWidth - spiderWidth) / 2;
+  const toP = (wallHeight - spiderHeight) / 2;
+
+  spider.style.left = Math.round(left) + 'px';
+  spider.style.top = Math.round(toP) + 'px';
+
+  spider.style.margin = '0';
+  spider.style.padding = '0';
+  spider.style.transform = 'none';
 }
 
+centerSpider();
+
 window.addEventListener('load', centerSpider);
+window.addEventListener('resize', centerSpider);
