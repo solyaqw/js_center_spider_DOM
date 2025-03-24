73d953215bb8b8581102684e8404d75e3cc1a89d
Solomiia Palka <solyaqw@gmail.com>
2025-03-24 13:23:48 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write code here
+const spider = document.querySelector('.spider');
+
+function centerSpider() {
+  spider.style.position = 'absolute';
+  spider.style.top = '50%';
+  spider.style.left = '50%';
+  spider.style.transform = 'translate(-50%, -50%)';
+}
+
+window.addEventListener('load', centerSpider);
